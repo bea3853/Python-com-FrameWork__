894d7f68e3f8560008d385bdd5d93c76bd788fad
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2026-05-16 17:47:50 -0300

    Add files via upload

---
+++ Aula 16-Polimorfismo/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import datetime
+
+
+
+st.title('Beatriz')
+
+st.caption('https://github.com/bea3853/')
+
+st.image('i.jpg')
+
+st.write('bea@gmail.com')
+
+st.caption('11565.65.66.')
+
+st.caption('Nova York')
+
+st.audio('t.mp3')
+
+    
+
+
+
+        
